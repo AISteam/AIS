c71b332f1953ec75e2dfa7f4903afae826d38b5e
eric <qinmudi@139.com>
2014-09-17 16:38:10 +0800

    update more

--- aiscore/test/mongo.js
+++ aiscore/test/mongo.js
@@ -23,7 +23,7 @@ db.open(function(err,db){
 
 		//更新数据
 		collection.update({'id':1},{$set:{'password':'admin','depart':'ad'}},{safe:true},function(err,result){
-			assert.equal(null,err);
+			//assert.equal(null,err);
 			console.log('更新数据的回调返回');
 			console.log(result);
 		});
@@ -32,16 +32,34 @@ db.open(function(err,db){
 		collection.find().toArray(function(err,docs){
 			console.log('find方法查找返回');
 			console.log(docs);
+			for(var index in docs){
+				console.log('姓名为：'+docs[index]['name']);
+			}
 		});
 
+		//查询并返回一个结果
 		collection.findOne(function(err,doc){
+			assert.equal(null,err);
 			console.log('findOne方法查找返回');
 			console.log(doc);
 		});
 
+		//有条件的查询
+		collection.find({'name':'ericqin','password':'admin'}).toArray(function(err,user){
+			console.log('查找用户名为ericqin，密码为admin的用户是否存在');
+			console.log(user);
+		});
+
+		//按条件查询并返回一个结果
+		collection.findOne({'id':1}, function(err, result) {
+			//assert.equal(null,err);
+			console.log('findOne有条件查询');
+			console.log(result);
+		});
+
 		//删除数据
 		collection.remove({'id':1},{safe:true},function(err,result){
-			assert.equal(null,err);
+			//assert.equal(null,err);
 			console.log('删除数据结果返回');
 			console.log(result);
 		});
@@ -50,6 +68,7 @@ db.open(function(err,db){
 			assert.equal(null,err);
 			console.log('删除数据结果返回');
 			console.log(result);
+			db.close();
 		});
 	});
 });
